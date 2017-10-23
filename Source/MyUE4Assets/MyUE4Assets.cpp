a6658daa16874a4131eae02895950a83a2e5012f
Newyork167 <newyork167@gmail.com>
2017-10-23 12:26:20 -0400

    Initial Commit

---
+++ Source/MyUE4Assets/MyUE4Assets.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyUE4Assets.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyUE4Assets, "MyUE4Assets" );
---
+++ Source/MyUE4Assets/MyUE4AssetsGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyUE4AssetsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYUE4ASSETS_API AMyUE4AssetsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
